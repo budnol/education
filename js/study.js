905e4116df947ca4354bd6d270eb17c4b5755d79
Denis <example@gmail.com>
2020-11-09 19:57:58 +0500

    addMovies

--- js/study.js
+++ js/study.js
@@ -1,46 +1,14 @@
 "use strict";
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      wrapper = document.querySelector('.wrapper'),
-      hearts = wrapper.querySelectorAll('.heart'),
-      oneHeart = wrapper.querySelector('.heart');
+// console.log(document.head);
+// console.log(document.documentElement);
 
-// box.style.backgroundColor = 'blue';
-// box.style.width = '500px';
+// console.log(document.body.childNodes);
 
-box.style.cssText = 'background-color: blue; width: 500px';
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
 
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
+// console.log(document.querySelector('#current').parentNode.parentNode);
+console.log(document.querySelector('#current').parentElement.parentElement);
 
-// for(let i = 0; i < hearts.length; i++){
-//     hearts[i].style.backgroundColor = 'blue';
-// }
-
-hearts.forEach(item => {
-    item.style.backgroundColor = 'blue';
-})
-
-const div = document.createElement('div');
-div.classList.add('black');
-
-wrapper.append(div);
-// wrapper.appendChild(div);
-
-// wrapper.prepend(div);
-// wrapper.insertBefore(div, hearts[0]);
-
-// hearts[0].before(div);
-// hearts[0].after(div);
-
-// circles[0].remove();
-// wrapper.removeChild(hearts[1]);
-
-// hearts[0].replaceWith(circles[0]);
-// wrapper.replaceChild(circles[0], hearts[0]);
-
-div.innerHTML = '<h1>Hello, world!</h1>';
-
-div. insertAdjacentHTML('afterend ', '<h2>Hello!</h2>');
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
